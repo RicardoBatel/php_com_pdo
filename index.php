ef3405bf0f0ec210de0694fcaa3023a62030b5e2
RIcardo Batel <ricardo.batel@hotmail.com>
2022-12-10 10:10:01 -0300

    Prepare Statement evitando problemas com SQL Injection

--- index.php
+++ index.php
@@ -9,20 +9,23 @@ if(!empty($_POST['usuario']) && !empty($_POST['senha'])) {
     try {
         $conexao = new PDO($dsn, $usuario, $senha);
         
-        $query = "select * from tb_usuarios where";
-        $query .= " email = '{$_POST['usuario']}' ";
-        $query .= " AND senha = '{$_POST['senha']}'";
+        $query = "select * from tb_usuarios where ";
+        $query .= " email = :usuario ";
+        $query .= " AND senha = :senha ";
 
-        echo $query;
+        $stmt = $conexao->prepare($query); //ele não executa diretamente a query, ele aguarda seu ok para executar
 
-        $stmt = $conexao->query($query);
+        $stmt->bindValue(':usuario', $_POST['usuario']);
+        $stmt->bindValue(':senha', $_POST['senha']);
+
+        $stmt->execute(); //executa a query depois do bindValue
 
         $usuario = $stmt->fetch();
-        echo '<hr>';
 
         echo '<pre>';
-            print_r($usuario);
+        print_r($usuario);
         echo '</pre>';
+        
     } catch (PDOException $e) {
         echo 'Erro: ' . $e->getCode() . ' Mensagem: ' . $e->getMessage();
         //registrar o erro de alguma forma.
